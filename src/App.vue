6ff7095d24839d12e1693d52a32a2bba7363642f
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-08-10 20:42:07 +0300

    slotsProps

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <template>
-  <div class='container pt-1'>
-    <div class='card'>
-      <h2>Slots</h2>
-    </div>
+     <div class='container pt-1'>
+        <div class='card'>
+            <h2>Динамические и асинхронные компоненты</h2>
+        </div>
 
-    <app-block>
-      <p>Это самый важный текст для нового блока</p>
-
-      <template #header>
-        <h3>Это заголовок!</h3>
-      </template>
-
-      <template v-slot:footer>
-        <hr />
-        <small>Это footer</small>
-      </template>
-    </app-block>
   </div>
 </template>
-
 <script>
-import AppBlock from './AppBlock.vue'
-
 export default {
-  mounted() {
-    console.log(this.$slots)
-  },
-  components: {
-    // 'app-news': AppNews
-    AppBlock
-  }
+
 }
 </script>
-
-<style scoped></style>
