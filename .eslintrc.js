8d325b5ac7b70feb54738479c5d3df893f0b6f0a
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-08-11 12:23:32 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,14 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    "Strings must use singlequote": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
     'no-unused-vars': 'off',
-    "no-irregular-whitespace": "off",
+    'no-irregular-whitespace': 'off',
     "no-mixed-spaces-and-tabs": "off",
     "indent": "off",
-   "Strings must use singlequote": "off"
+   
   }
 }
